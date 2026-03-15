735057f988a3b2e65c668bb9e995954aa4c3ea71
melroy <melroysesu@gmail.com>
2026-03-15 22:18:45 +0530

    contact changes

--- Nostra-Ecommerce/contact.js
+++ Nostra-Ecommerce/contact.js
@@ -9,3 +9,21 @@ menuicon.addEventListener("click",function(){
 closebtn.addEventListener("click",function(){
     sidenav.style.marginLeft="-60%"
 })
+
+
+document.querySelector("form").addEventListener("submit", function(e){
+
+let name = document.getElementById("name").value.trim();
+let email = document.getElementById("email").value.trim();
+let message = document.getElementById("message").value.trim();
+
+if(name === "" || email === "" || message === ""){
+    alert("Please fill all the fields");
+    e.preventDefault(); // stop submit if empty
+}
+else{
+    alert("Form Submitted Successfully ✅");
+    // form will submit and page will refresh automatically
+}
+
+});
